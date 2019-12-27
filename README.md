cd982ed323f1dd683f1b4ad4aace0eef721f82ea
Tabll <tabll@outlook.com>
2019-12-27 11:14:53 +0800

    添加 Read me

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tabll API
+
+[![pipeline status](https://gitlab.tabll.cn/Tabll/tabllapi/badges/develop/pipeline.svg)](https://gitlab.tabll.cn/Tabll/tabllapi/commits/develop)
+[![coverage report](https://gitlab.tabll.cn/Tabll/tabllapi/badges/develop/coverage.svg)](https://gitlab.tabll.cn/Tabll/tabllapi/commits/develop)
+
+这里是网站的一些简单的API，目前仅以学习为主
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,10 @@ use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API 路由
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| 这里是应用程序注册 API 路由的地方
 |
 */
 
